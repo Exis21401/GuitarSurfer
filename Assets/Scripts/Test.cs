267f30704ca23cbc9483f9ac43d64342b3806014
Exis21401 <alexis.sosa.gracia@gmail.com>
2019-04-30 21:47:55 +0200

    Player Movement Acceleration

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections; // Librería de funcionalidades de Unity
+using System.Collections.Generic; // Librería de funcionalidades de Unity
+using UnityEngine; //Usando el motor de Unity
+using TMPro; //Usar el TextMesh Pro
+
+public class Test : MonoBehaviour
+{
+    public float velocity; public bool canRun; public string playerName; public int currentLevel; int numerador; int denominador;
+
+    // Lo primero que se ejecuta
+    private void Awake() { 
+        
+    }
+
+    // Start is called before the first frame update
+    void Start() { 
+        canRun = false; velocity = 20f; playerName = "Alexis"; numerador = 4; denominador = 2;
+
+        Debug.Log("Numerador: " + numerador);
+        Debug.Log("Denominador: " + denominador);
+
+        if(denominador != 0 && numerador >= denominador){
+            Debug.Log("Resultado: " + numerador / denominador);
+        }
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+
+    private void LateUpdate(){
+        
+    }
+}
---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    #region VARIABLES
+    public float speed;
+    public float maxSpeed;
+    public float acceleration;
+    public float deceleration;
+    float right;
+    float forward;
+    Rigidbody rb;
+    Vector3 vel;
+    #endregion
+
+    #region START AND UPDATE
+    // Start is called before the first frame update
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        rb.velocity = vel;
+        Movement();
+        Accelerate();
+    }
+    #endregion
+
+    #region MOVEMENT AND ACCELERATION
+    void Movement() {
+
+        if (Input.GetKey(KeyCode.W)){
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, speed);
+        }
+        if (Input.GetKey(KeyCode.A)){
+            rb.velocity = new Vector3(-speed, rb.velocity.y, rb.velocity.z);
+        }
+        if (Input.GetKey(KeyCode.S)){
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, -speed);
+        }
+        if (Input.GetKey(KeyCode.D)){
+            rb.velocity = new Vector3(speed, rb.velocity.y, rb.velocity.z);
+        }
+
+     }
+
+    void Accelerate(){
+        forward = Input.GetAxis("Forward");
+        right = Input.GetAxis("Right");
+
+        if(forward != 0 || right != 0)
+        {
+            speed += acceleration * Time.deltaTime;
+            speed = Mathf.Clamp(speed, 0, maxSpeed);
+        }
+        else
+        {
+            speed -= deceleration * Time.deltaTime;
+            speed = Mathf.Clamp(speed, 0, maxSpeed);
+        }
+
+        /*if(Input.GetAxisRaw("Right") != 0){
+            vel.x = acceleration * right * Time.deltaTime;
+        }
+
+        else if(Input.GetAxisRaw("Right") == 0){
+            vel.x = deceleration * right * Time.deltaTime;
+        }
+
+        if (Input.GetAxisRaw("Forward") != 0)
+        {
+            vel.z = acceleration * forward * Time.deltaTime;
+        }
+
+        else if (Input.GetAxisRaw("Forward") == 0)
+        {
+            vel.z = deceleration * forward * Time.deltaTime;
+        }
+
+        vel.x = Mathf.Clamp(vel.x, -speed, speed);
+        vel.z = Mathf.Clamp(vel.z, -speed, speed);*/
+    }
+    #endregion
+
+}
